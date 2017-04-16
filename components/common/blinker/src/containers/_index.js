018039cec4efd490e2a4d19d555efb0cfad0f961
Denghejun <hjdeng@CNhjdeng.local>
2017-04-16 22:45:59 +0800

    remove no used code

--- components/common/blinker/src/containers/_index.js
+++ components/common/blinker/src/containers/_index.js
@@ -1,4 +1,1 @@
 export { default as BlinkerContainer } from './blinkerContainer'
-// import BlinkerContainerClass from './blinkerContainer'
-// const BlinkerContainer = BlinkerContainerClass.connect()
-// export { BlinkerContainer }
