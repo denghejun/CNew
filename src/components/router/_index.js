8cf6f1306178aaaf16603beadb3079e628505a03
Denghejun <hjdeng@CNhjdeng.local>
2017-05-15 00:30:53 +0800

    remove style

--- src/components/router/_index.js
+++ src/components/router/_index.js
@@ -10,9 +10,9 @@ export default class CNewApp extends React.Component {
                 <Scene key='root'>
                     <Scene key='cnew' duration={300} component={Components.cnew} initial={true} hideNavBar={true} />
                     <Scene key='movie' renderBackButton={() => { }} component={Components.movie} navigationBarStyle={Styles.scene.navigationBarStyle} hideNavBar={false}>
-                        <Scene key='movie_showing' title='正在上映' navigationBarStyle={Styles.scene.navigationBarStyle} />
-                        <Scene key='movie_coming' title='即将上映' navigationBarStyle={Styles.scene.navigationBarStyle} />
-                        <Scene key='movie_search' title='电影搜索' navigationBarStyle={Styles.scene.navigationBarStyle} />
+                        <Scene key='movie_showing' title='正在上映' />
+                        <Scene key='movie_coming' title='即将上映' />
+                        <Scene key='movie_search' title='电影搜索' />
                     </Scene>
                 </Scene>
             </Router>
